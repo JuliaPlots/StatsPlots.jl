2a2396aaa26c3d54733599d7596e8942808de622
Thomas Breloff <tom@breloff.com>
2016-08-18 15:37:59 -0400

    cornerplot redo

--- src/cornerplot.jl
+++ src/cornerplot.jl
@@ -1,119 +1,107 @@
 @userplot CornerPlot
-# adapted from CorrPlot
 
-function update_ticks_guides(d::KW, labs, i, j, n, k)
-    d[:xticks] = (i==n-1)
-    d[:yticks] = (j==2)
-    d[:xguide] = (i==n-1 && j>1 ? cycle(labs,j-1) : "")
-    d[:yguide] = (j==2 && i<n ? cycle(labs,i+k) : "")
-end
-
-@recipe function f(cp::CornerPlot; compact=false, maxvariables=30)
-    # Plots.dumpdict(d, "corner", true)
+@recipe function f(cp::CornerPlot; compact=false, maxvariables=30, histpct = 0.1)
     mat = cp.args[1]
+    @assert typeof(mat) <: AbstractMatrix
     N = size(mat,2)
-    nsamples = size(mat,1)
-    if N>maxvariables
+    if N > maxvariables
         error("Requested to plot $N variables in $(N^2) subplots!  Likely, the first input needs transposing, otherwise increase maxvariables.")
     end
-    delete!(d, :maxvariables)
-    labs = pop!(d, :label, [""])
+
+    # k is the number of rows/columns to hide
+    k = compact ? 1 : 0
+
+    # n is the total number of rows/columns.  hists always shown
+    n = N + 1 - k
+
+    labs = pop!(d, :label, ["x$i" for i=1:N])
     if labs!=[""] && length(labs)!=N
         error("Number of labels not identical to number of datasets")
     end
-    n = compact ? N : N+1 # nxn is the plot-grid size
-    colrange = 1:n-1
-    rowrange = 2:n
 
-    delete!(d, :compact)
-
-    # Make the layout.  Note that the subplots are numbered in
-    # row-major order.
-    cell = GridLayout(n,n)
-    sz = 1/4n
+    # build a grid layout, where the histogram sizes are a fixed percentage, and we
+    scatterpcts = ones(n-1) * (1-histpct) / (n-1)
+    g = grid(n, n, widths = vcat(scatterpcts, histpct), heights = vcat(histpct, scatterpcts))
+    spidx = 1
     indices = zeros(Int,n,n)
-    pltnr = 1
-    for i=1:n
-        for j=1:n
-            if j==1 # first column: hists
-                if i<n
-                    indices[i,j] = pltnr; pltnr +=1
-                    cell[i,j] = EmptyLayout(width=sz*pct, height=:auto)
-                else # bottom left: empty
-                    cell[i,j] = EmptyLayout(width=sz*pct, height=sz*pct, blank=true)
-                end
-            elseif i==n # last row: hists
-                indices[i,j] = pltnr; pltnr +=1
-                cell[i,j] = EmptyLayout(height=sz*pct, width=:auto)
-            elseif compact && j-1>i # top right empty if compact
-                cell[i,j] = EmptyLayout(width=:auto, height=:auto, blank=true)
-            else
-                indices[i,j] = pltnr; pltnr +=1
-                cell[i,j] = EmptyLayout(width=:auto, height=:auto)
-            end
+    for i=1:n, j=1:n
+        isblank = (i==1 && j==n) || (compact && i>1 && j<n && j>=i)
+        g[i,j].attr[:blank] = isblank
+        if !isblank
+            indices[i,j] = spidx
+            spidx += 1
         end
     end
-    layout := cell
-    legend := false
+    layout := g
+
+    # some defaults
+    legend    := false
     foreground_color_border := nothing
-    margin := 1mm
-    titlefont := font(11)
-    fillcolor := :black
+    margin    --> 1mm
+    titlefont --> font(11)
+    fillcolor --> :black
+    linecolor --> :black
+    grid      --> true
+    ticks     := nothing
+    formatter := v->""
+    link      := :both
 
     # figure out good defaults for scatter plot dots:
-    pltarea = 1/2n
-    ms = pltarea*1000/sqrt(nsamples)
-    markersize --> max(min(10, ms), 0.5)
-    ma = pltarea*100/nsamples^0.42
-    markeralpha --> max(min(0.4, ma), 0.05)
+    pltarea = 1 / (2n)
+    nsamples = size(mat,1)
+    markersize  --> clamp(pltarea * 1000 / sqrt(nsamples), 1,     10)
+    markeralpha --> clamp(pltarea * 100 / nsamples^0.42,   0.005, 0.4)
 
-    # histograms in the left column
-    k = compact ? 1 : 0
-    for i=1:n-1
+    # histograms in the right column
+    for i=1:N
+        compact && i == 1 && continue
         @series begin
             orientation := :h
-            xflip := true
-            link := :none
-            seriestype := :histogram
-            subplot := indices[i,1]
-            grid := false
-            xticks := false
-            yticks := false
-            sub(mat,:,i+k)
+            seriestype  := :histogram
+            subplot     := indices[i+1-k,n]
+            grid        := false
+            view(mat,:,i)
         end
     end
-    # histograms in the bottom row
-    for j=2:n
+
+    # histograms in the top row
+    for j=1:N
+        compact && j == N && continue
         @series begin
-            yflip := true
-            seriestype := :histogram
-            subplot := indices[end,j]
-            grid := false
-            xticks := false
-            yticks := false
-            sub(mat,:,j-1)
+            seriestype  := :histogram
+            subplot     := indices[1,j]
+            grid        := false
+            view(mat,:,j)
         end
     end
 
     # scatters
-    for i=1:n-1
-        vi = sub(mat,:,i+k)
-        tmp = vec(indices[i,:])
-        ylink := tmp[tmp.>0] # remove 0s
-        for j = 2:n
-            if !(compact && j-1>i) # top right empty if compact
-                vj = sub(mat,:,j-1)
-                subplot := indices[i,j]
-                tmp = vec(indices[:,j])
-                xlink := tmp[tmp.>0]
-                update_ticks_guides(d, labs, i, j, n, k)
-                @series begin
-                    seriestype := :scatter
-                    smooth := true
-                    markerstrokewidth --> 0
-                    vj, vi
+    for i=1:N
+        vi = view(mat,:,i)
+        for j = 1:N
+            # only the lower triangle
+            if compact && i <= j
+                continue
+            end
+
+            vj = view(mat,:,j)
+            @series begin
+                ticks := :auto
+                if i == N
+                    xformatter := :auto
+                    xguide := cycle(labs, j)
                 end
+                if j == 1
+                    yformatter := :auto
+                    yguide := cycle(labs, i)
+                end
+                seriestype := :scatter
+                subplot := indices[i+1-k, j]
+                smooth := true
+                markerstrokewidth --> 0
+                vj, vi
             end
         end
+        # end
     end
 end
