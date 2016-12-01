1fc56ecc01355ac8f835b9e05fbd87b7d2ce44c3
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2016-12-01 18:40:50 +0100

    create a vector instead of a Matrix

--- src/dataframes.jl
+++ src/dataframes.jl
@@ -8,8 +8,7 @@ end
 # if it's an array of symbols, set the labels and return a Vector{Any} of columns
 function handle_dfs(df::AbstractDataFrame, d::KW, letter, syms::AbstractArray{Symbol})
     get!(d, :label, reshape(syms, 1, length(syms)))
-    ret = Any[collect(df[s]) for s in syms]
-    hcat(ret...)
+    vec(Any[collect(df[s]) for s in syms])
 end
 
 # for anything else, no-op
