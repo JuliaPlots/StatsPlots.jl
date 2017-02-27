057c1bdc4a57c7ebd00b2f562022757a9b6da124
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-02-27 11:47:52 +0100

    replace NullableArrays with DataTables

--- src/dataframes.jl
+++ src/dataframes.jl
@@ -1,7 +1,7 @@
 
-toArray{T<:Any}(na::NullableArrays.NullableArray{T,1}) = convert(Array, na)
-#perhaps? toArray{T<:String}(na::NullableArrays.NullableArray{T,1}) = convert(String, na, "")
-toArray{T<:Number}(na::NullableArrays.NullableArray{T,1}) = Float64[isnull(x) ? NaN : get(x) for x in na]
+toArray{T<:Any}(na::DataTables.NullableArray{T,1}) = convert(Array, na)
+#perhaps? toArray{T<:String}(na::DataTables.NullableArray{T,1}) = convert(String, na, "")
+toArray{T<:Number}(na::DataTables.NullableArray{T,1}) = Float64[isnull(x) ? NaN : get(x) for x in na]
 
 # if it's one symbol, set the guide and return the column
 function handle_dfs(df::AbstractDataTable, d::KW, letter, sym::Symbol)
