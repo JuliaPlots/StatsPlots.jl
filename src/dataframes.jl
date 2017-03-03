d3b14ba70a9e82872e6e836d2a4c1d8cac50edaa
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-03-03 12:09:27 +0100

    fix expressions as colmatrix

--- src/dataframes.jl
+++ src/dataframes.jl
@@ -33,7 +33,19 @@ Base.hcat(a::Number, b::AbstractArray) = hcat(fill(a, size(b,1)), b)
 # allows the passing of expressions including DataFrame columns as symbols
 processDFarg(df::AbstractDataFrame, sym::Symbol) = collect(df[sym])
 function processDFarg(df::AbstractDataFrame, syms::AbstractMatrix)
+    ret = processDFcol(df, syms[:,1])
+    for i in 2:size(syms, 2)
+        tmp = processDFcol(df, syms[:,i])
+        ret = hcat(ret, tmp)
+    end
+    ret
+end
+
 processDFarg(df::AbstractDataFrame, expr::Expr) = eval(processDFsym(df, expr))
+processDFarg(df::AbstractDataFrame, anything) = anything
+
+processDFcol{T <: Union{Expr, Symbol}}(df, col::AbstractArray{T}) = processDFarg(df, col[1])
+processDFcol(df, anything) = anything
 
 # the processDFsym! functions work with expressions and pass results to processDFarg for final eval - this to allow recursion without eval
 processDFsym(df::AbstractDataFrame, s::Symbol) = haskey(df,s) ? :(collect($(df)[$s])) : :($s)
@@ -51,7 +63,7 @@ end
 # if a DataFrame is the first arg, lets swap symbols out for columns
 @recipe function f(df::AbstractDataFrame, args...)
     # if any of these attributes are symbols, swap out for the df column
-    for k in (:fillrange, :line_z, :marker_z, :markersize, :ribbon, :weights, :xerror, :yerror, :hover)
+    for k in (:fillrange, :line_z, :marker_z, :markersize, :ribbon, :weights, :xerror, :yerror, :hover, :bar_width, :fill_z, :fillalpha, :fillcolor, :linealpha, :linecolor, :linestyle, :linewidth, :markeralpha, :markercolor, :markershape, :markerstrokecolor, :series_annotations, :seriesalpha, :seriescolor)
         if haskey(d, k)
             d[k] = processDFarg(df, d[k])
         end
