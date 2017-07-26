5f8f8f19f0de81a76e72035af28cbbcac0a7343b
Timothée Poisot <timothee.poisot@umontreal.ca>
2017-07-26 11:35:14 -0400

    :construction: it should work - but it doesn't

--- src/beeswarm.jl
+++ src/beeswarm.jl
@@ -2,52 +2,63 @@
 
 # ---------------------------------------------------------------------------
 # Beeswarm plot
-@recipe function f(::Type{Val{:beeswarm}}, x, y, z; trim::Bool=false, side::Symbol=:both)
+@recipe function f(::Type{Val{:beeswarm}}, x, y, z; trim=false, side=:both)
 
   side = check_side(side)
 
-  x, y = Float64[], Float64[]
+  xp, yp = Float64[], Float64[]
   glabels = sort(collect(unique(x)))
   bw = d[:bar_width]
   bw == nothing && (bw = 0.8)
 
   for (i,glabel) in enumerate(glabels)
-
     # We get the values for this label
     lab_y = y[filter(i -> _cycle(x,i) == glabel, 1:length(y))]
     lab_x = zeros(lab_y)
 
-    # Then we apply Sturge's rule to get the number of bins
-    n = convert(Int64, ceil(1+log2(length(lab_y))))
+    # Number of bins (defaults to sturges)
+    binning_mode = d[:bins]
+    if binning_mode == :auto
+      binning_mode = :sturges
+    end
+    n = Plots._auto_binning_nbins(tuple(lab_y), 1, mode=binning_mode)
 
     # Get the widths and the coordinates
-    widths, centers = violin_coords(lab_y, trim=trim, n=n)
+    widths, centers = StatPlots.violin_coords(lab_y, trim=trim, n=n)
     isempty(widths) && continue
 
     # normalize
-    hw = 0.5_cycle(bw, i)
+    hw = 0.5Plots._cycle(bw, i)
     widths = hw * widths / Plots.ignorenan_maximum(widths)
 
     # make the violin
     xcenter = Plots.discrete_value!(d[:subplot][:xaxis], glabel)[1]
 
-    if (side==:right)
-      xcoords = vcat(widths, zeros(length(widths))) + xcenter
-    elseif (side==:left)
-      xcoords = vcat(zeros(length(widths)), -reverse(widths)) + xcenter
-    else
-      xcoords = vcat(widths, -reverse(widths)) + xcenter
+    for i in 2:length(centers)
+      inside = Bool[centers[i-1] < u <= centers[i] for u in lab_y]
+      if sum(inside) > 1
+        if (side==:right)
+          start == 0.0
+          stop = widths[i]
+        elseif (side==:left)
+          start = -widths[i]
+          stop = 0.0
+        elseif (side == :both)
+          start = -widths[i]
+          stop = widths[i]
+        end
+        lab_x[inside] = lab_x[inside] .+ linspace(start, stop, sum(inside)) .+ xcenter
+      end
     end
 
-    ycoords = vcat(centers, reverse(centers))
+    append!(xp, lab_x)
+    append!(yp, lab_y)
 
-    push!(xsegs, xcoords)
-    push!(ysegs, ycoords)
   end
 
+  x := xp
+  y := yp
   seriestype := :scatter
-  x := xsegs.pts
-  y := ysegs.pts
   ()
 
 end
