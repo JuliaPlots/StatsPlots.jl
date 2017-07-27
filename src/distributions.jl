6e16f1a3ea5bc8ad7bc5da8e4567395832054598
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-07-27 22:43:03 +0200

    Let adapted_grid construct xs

--- src/distributions.jl
+++ src/distributions.jl
@@ -2,15 +2,14 @@
 # pick a nice default x range given a distribution
 function default_range(dist::Distribution, n = 4)
     μ, σ = mean(dist), std(dist)
-    linspace(max(minimum(dist), μ - n*σ),
-             min(maximum(dist), μ + n*σ), 100)
+    max(minimum(dist), μ - n*σ),
+             min(maximum(dist), μ + n*σ)
 end
 
 # this "user recipe" adds a default x vector based on the distribution's μ and σ
-@recipe f(dist::Distribution) = (dist, default_range(dist))
+@recipe f(dist::Distribution) = (dist, default_range(dist)...)
 
 # this "type recipe" replaces any instance of a distribution with a function mapping xi to yi
 @recipe function f{T<:Distribution}(::Type{T}, dist::T; func = pdf)
     xi -> func(dist, xi)
 end
-
