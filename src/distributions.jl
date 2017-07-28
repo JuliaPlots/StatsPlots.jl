f5a7cd20f35524de2d991ac8a34a243d7b27b813
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-07-28 15:56:28 +0200

    Edit default_range to use the 99th quantile

--- src/distributions.jl
+++ src/distributions.jl
@@ -1,9 +1,9 @@
 
 # pick a nice default x range given a distribution
-function default_range(dist::Distribution, n = 4)
-    μ, σ = mean(dist), std(dist)
-    max(minimum(dist), μ - n*σ),
-             min(maximum(dist), μ + n*σ)
+function default_range(dist::Distribution, alpha = 0.01)
+    minval = isfinite(minimum(dist)) ? minimum(dist) : quantile(dist, alpha)
+    maxval = isfinite(maximum(dist)) ? maximum(dist) : quantile(dist, 1-alpha)
+    minval, maxval
 end
 
 # this "user recipe" adds a default x vector based on the distribution's μ and σ
