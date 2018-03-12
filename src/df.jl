d34719c4264825db7f120a091f2a186b1bbaa969
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-03-12 10:22:21 -0600

    Fix hygiene issue in @df (#131)

--- src/df.jl
+++ src/df.jl
@@ -11,8 +11,22 @@ for strings and symbols respectively.
 `x` can be either a plot command or a block of plot commands.
 """
 macro df(d, x)
+    esc(df_helper(d, x))
+end
+
+"""
+    `@df x`
+
+Curried version of `@df d x`. Outputs an anonymous function `d -> @df d x`.
+"""
+macro df(x)
+    i = gensym()
+    esc(Expr(:(->), i, df_helper(i, x)))
+end
+
+function df_helper(d, x)
     if isa(x, Expr) && x.head == :block
-        commands = [:(@df($(esc(d)), $(esc(xx)))) for xx in x.args if !(isa(xx, Expr) && xx.head == :line)]
+        commands = [:(@df($d, $x)) for xx in x.args if !(isa(xx, Expr) && xx.head == :line)]
         return Expr(:block, commands...)
     elseif isa(x, Expr) && x.head == :call
         syms = Any[]
@@ -27,20 +41,10 @@ macro df(d, x)
         else
             label_plot_call = Expr(:call, :(StatPlots.add_label), argnames, plot_call.args...)
         end
-        return esc(Expr(:block, compute_vars, label_plot_call))
+        return Expr(:block, compute_vars, label_plot_call)
     else
         error("Second argument can only be a block or function call")
     end
-end
-
-"""
-    `@df x`
-
-Curried version of `@df d x`. Outputs an anonymous function `d -> @df d x`.
-"""
-macro df(x)
-    i = gensym()
-    :($i -> @df($i, $x))
 end
 
 parse_iterabletable_call!(d, x, syms, vars) = x
@@ -74,8 +78,8 @@ not_kw(x) = true
 not_kw(x::Expr) = !(x.head in [:kw, :parameters])
 
 function insert_kw!(x::Expr, s::Symbol, v)
-    index = isa(x.args[2], Expr) && x.args[2].head == :parameters ? 3 : 2		
-    x.args = vcat(x.args[1:index-1], Expr(:kw, s, v), x.args[index:end])		
+    index = isa(x.args[2], Expr) && x.args[2].head == :parameters ? 3 : 2
+    x.args = vcat(x.args[1:index-1], Expr(:kw, s, v), x.args[index:end])
 end
 
 _arg2string(d, x) = stringify(x)
@@ -107,10 +111,10 @@ function add_label(argnames, f, args...; kwargs...)
 end
 
 get_col_from_dict(s::Int, col_dict, name2index) = col_dict[s]
-get_col_from_dict(s::Symbol, col_dict, name2index) = 
+get_col_from_dict(s::Symbol, col_dict, name2index) =
     haskey(name2index, s) ? col_dict[name2index[s]] : s
 
-get_col_from_dict(s, col_dict, name2index) = 
+get_col_from_dict(s, col_dict, name2index) =
     hcat(get_col_from_dict.(s, col_dict, name2index)...)
 
 function extract_columns_from_iterabletable(df, syms...)
@@ -118,7 +122,7 @@ function extract_columns_from_iterabletable(df, syms...)
     iter = getiterator(df)
     name2index = Dict(zip(column_names(iter), 1:length(column_names(iter))))
 
-    col_indices = union(Iterators.filter(t -> t != 0, 
+    col_indices = union(Iterators.filter(t -> t != 0,
         (isa(s, Integer) ? s : get(name2index, s, 0) for s in vcat(syms...))))
     col_values = create_columns_from_iterabletable(df, col_indices)[1]
     col_dict = Dict(zip(col_indices, [convert_missing.(t) for t in col_values]))
