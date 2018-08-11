21ab1953574135f6ce44ddecd663f12a459a89fc
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-08-11 16:49:06 +0100

    fix julia 1 (#163)

--- src/StatPlots.jl
+++ src/StatPlots.jl
@@ -19,7 +19,7 @@ import Clustering: Hclust
 
 import KernelDensity
 @recipe f(k::KernelDensity.UnivariateKDE) = k.x, k.density
-@recipe f(k::KernelDensity.BivariateKDE) = k.x, k.y, k.density.'
+@recipe f(k::KernelDensity.BivariateKDE) = k.x, k.y, permutedims(k.density)
 
 @shorthands cdensity
 
