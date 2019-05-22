375ca0d339e5bf1d71edb5a1a78b5509f8c78beb
Kevin Bonham <kevbonham@gmail.com>
2019-05-22 12:50:39 -0400

    Allow user to select mds axes, and allow z axis

--- src/mds_scatter.jl
+++ src/mds_scatter.jl
@@ -1,10 +1,22 @@
-@recipe function f(mds::MDS{<:Real})
-    xticks := false
-    yticks := false
+@recipe function f(mds::MDS{<:Real}; mds_axes=(1,2))
+    length(mds_axes) in [2,3] || throw(ArgumentError("Can only accept 2 or 3 mds axes"))
+    xax = mds_axes[1]
+    yax = mds_axes[2]
     ev = eigvals(mds)
     var_explained = [v / sum(ev) for v in ev]
-    xlabel --> "MDS1 ($(round(var_explained[1] * 100, digits = 2))%)"
-    ylabel --> "MDS2 ($(round(var_explained[2] * 100, digits = 2))%)"
+    proj = projection(mds)
+
+    xlabel --> "MDS$xax ($(round(var_explained[xax] * 100, digits = 2))%)"
+    ylabel --> "MDS$yax ($(round(var_explained[yax] * 100, digits = 2))%)"
     seriestype := :scatter
-    projection(mds)[:,1], projection(mds)[:,2]
+    xticks := false
+    yticks := false
+
+    if length(mds_axes) == 3
+        zax = mds_axes[3]
+        zlabel --> "MDS$zax ($(round(var_explained[zax] * 100, digits = 2))%)"
+        proj[:,xax], proj[:,yax], proj[:,zax]
+    else
+        proj[:,xax], proj[:,yax]
+    end
 end
