b932d985e3549c3a25d9973219c0107e44d8a688
Kevin Bonham <kevbonham@gmail.com>
2019-06-21 14:26:45 -0400

    scaling aspect_ratio = 1. FINE. I'm not mad :-P

--- src/ordinations.jl
+++ src/ordinations.jl
@@ -4,20 +4,19 @@
     yax = mds_axes[2]
     ev = eigvals(mds)
     var_explained = [v / sum(ev) for v in ev]
-    proj = projection(mds)
+    tfm = collect(transform(mds)')
 
     xlabel --> "MDS$xax ($(round(var_explained[xax] * 100, digits = 2))%)"
     ylabel --> "MDS$yax ($(round(var_explained[yax] * 100, digits = 2))%)"
     seriestype := :scatter
-    xticks --> false
-    yticks --> false
+    aspect_ratio --> 1
 
     if length(mds_axes) == 3
         zax = mds_axes[3]
         zlabel --> "MDS$zax ($(round(var_explained[zax] * 100, digits = 2))%)"
-        proj[:,xax], proj[:,yax], proj[:,zax]
+        tfm[:,xax], tfm[:,yax], tfm[:,zax]
     else
-        proj[:,xax], proj[:,yax]
+        tfm[:,xax], tfm[:,yax]
     end
 end
 
