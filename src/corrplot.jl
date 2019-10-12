8ec95eebea0307491ad791794f5eb17f4ba92fe3
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-10-11 20:44:53 -0400

    Add docstring to corrplot

--- src/corrplot.jl
+++ src/corrplot.jl
@@ -1,3 +1,24 @@
+"""
+    corrplot
+
+This plot type shows the correlation among input variables.  
+A correlation plot may be produced by a matrix.
+
+
+A correlation matrix can also be created from the columns of a `DataFrame`
+using the [`@df`](@ref) macro like so:
+
+```julia
+@df iris corrplot([:SepalLength :SepalWidth :PetalLength :PetalWidth])
+```
+
+The marker color in scatter plots reveals the degree of correlation. 
+Pass the desired colorgradient to `markercolor`.
+
+With the default gradient positive correlations are blue, neutral are yellow 
+and negative are red. In the 2d-histograms, the color gradient shows the frequency 
+of points in that bin (as usual, controlled by `seriescolor`).
+"""
 @userplot CorrPlot
 
 recipetype(::Val{:corrplot}, args...) = CorrPlot(args)
