e4b9b3a67ad26f2a984db8314735407a94b7f611
Kevin Bonham <kevbonham@gmail.com>
2019-10-19 10:41:44 -0400

    remove PCA

--- src/ordinations.jl
+++ src/ordinations.jl
@@ -20,24 +20,7 @@
     end
 end
 
+#= This needs to wait on a different PCA API in MultivariateStats.jl
 @recipe function f(pca::PCA{<:Real}; pca_axes=(1,2))
-    length(pca_axes) in [2,3] || throw(ArgumentError("Can only accept 2 or 3 pca axes"))
-    xax = pca_axes[1]
-    yax = pca_axes[2]
-    vars = principalvars(pca)
-    var_explained = [v / sum(vars) for v in vars] .* principalratio(pca)
-    proj = projection(pca)
-
-    xlabel --> "PCA$xax"
-    ylabel --> "PCA$yax"
-    seriestype := :scatter
-    aspect_ratio --> 1
-
-    if length(pca_axes) == 3
-        zax = pca_axes[3]
-        zlabel --> "PCA$zax"
-        proj[:,xax], proj[:,yax], proj[:,zax]
-    else
-        proj[:,xax], proj[:,yax]
-    end
 end
+#=
