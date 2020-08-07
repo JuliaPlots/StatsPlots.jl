e0a506e669d5ba7ba1a5478fc218a9a2375b1648
Simon Christ <SimonChrist@gmx.de>
2020-08-07 17:04:58 +0200

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,4 +12,12 @@ using NaNMath
     gpl = groupedhist( rand(rng, 1000), yscale=:log10, ylims=(1e-2, 1e4), bar_position = :dodge)
     @test NaNMath.minimum(gpl[1][1][:y]) <= 1e-2
     @test NaNMath.minimum(gpl[1][1][:y]) > 0
+
+
+    data = [1,1,1,1,2,1]
+    mask = (collect(1:6) .< 5)
+    gpl1 = groupedhist(data[mask], group = mask[mask], color = 1)
+    gpl2 = groupedhist(data[.!mask], group = mask[.!mask], color = 2)
+    gpl12 = groupedhist(data, group = mask, nbins = 5, bar_position = :stack)
+    @test NaNMath.maximum(gpl12[1][2][:y]) == NaNMath.maximum(gpl1[1][1][:y]) 
 end # testset
