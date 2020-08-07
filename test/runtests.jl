cc31d0310181f2217c859646905596c5daeae24a
Simon Christ <SimonChrist@gmx.de>
2020-08-07 16:21:59 +0200

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using StatsPlots
 using Test
+using StableRNGs
+using NaNMath
 
-# write your own tests here
-@test 1 == 1
+@testset "Grouped histogram" begin
+    rng = StableRNG(1337)
+    gpl = groupedhist( rand(rng, 1000), yscale=:log10, ylims=(1e-2, 1e-4))
+    @test NaNMath.minimum(gpl[1][1][:y]) <= 1e-2
+    @test NaNMath.minimum(gpl[1][1][:y]) > 0
+end # testset
