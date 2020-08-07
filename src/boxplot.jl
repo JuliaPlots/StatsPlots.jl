8a035ff7a93d015862dc8a98955e250e861db697
Diego Javier Zea <diegozea@gmail.com>
2020-08-07 05:03:42 +0200

    Better hover text in plotly for boxplots
    This add hover text for the boxplot.

--- src/boxplot.jl
+++ src/boxplot.jl
@@ -4,7 +4,16 @@
 
 notch_width(q2, q4, N) = 1.58 * (q4-q2)/sqrt(N)
 
-@recipe function f(::Type{Val{:boxplot}}, x, y, z; notch=false, range=1.5, outliers=true, whisker_width=:match)
+@recipe function f(
+    ::Type{Val{:boxplot}},
+    x,
+    y,
+    z;
+    notch = false,
+    range = 1.5,
+    outliers = true,
+    whisker_width = :match,
+)
     # if only y is provided, then x will be UnitRange 1:size(y,2)
     if typeof(x) <: AbstractRange
         if step(x) == first(x) == 1
@@ -14,6 +23,7 @@ notch_width(q2, q4, N) = 1.58 * (q4-q2)/sqrt(N)
         end
     end
     xsegs, ysegs = Segments(), Segments()
+    texts = String[]
     glabels = sort(collect(unique(x)))
     warning = false
     outliers_x, outliers_y = zeros(0), zeros(0)
@@ -21,18 +31,18 @@ notch_width(q2, q4, N) = 1.58 * (q4-q2)/sqrt(N)
     bw == nothing && (bw = 0.8)
     @assert whisker_width == :match || whisker_width >= 0 "whisker_width must be :match or a positive number"
     ww = whisker_width == :match ? bw : whisker_width
-    for (i,glabel) in enumerate(glabels)
+    for (i, glabel) in enumerate(glabels)
         # filter y
-        values = y[filter(i -> _cycle(x,i) == glabel, 1:length(y))]
+        values = y[filter(i -> _cycle(x, i) == glabel, 1:length(y))]
 
         # compute quantiles
-        q1,q2,q3,q4,q5 = quantile(values, Base.range(0,stop=1,length=5))
+        q1, q2, q3, q4, q5 = quantile(values, Base.range(0, stop = 1, length = 5))
 
         # notch
         n = notch_width(q2, q4, length(values))
 
         # warn on inverted notches?
-        if notch && !warning && ( (q2>(q3-n)) || (q4<(q3+n)) )
+        if notch && !warning && ((q2 > (q3 - n)) || (q4 < (q3 + n)))
             @warn("Boxplot's notch went outside hinges. Set notch to false.")
             warning = true # Show the warning only one time
         end
@@ -49,7 +59,7 @@ notch_width(q2, q4, N) = 1.58 * (q4-q2)/sqrt(N)
 
         # outliers
         if Float64(range) != 0.0  # if the range is 0.0, the whiskers will extend to the data
-            limit = range*(q4-q2)
+            limit = range * (q4 - q2)
             inside = Float64[]
             for value in values
                 if (value < (q2 - limit)) || (value > (q4 + limit))
@@ -65,29 +75,77 @@ notch_width(q2, q4, N) = 1.58 * (q4-q2)/sqrt(N)
             # using maximum and minimum values inside the limits
             q1, q5 = Plots.ignorenan_extrema(inside)
             q1, q5 = (min(q1, q2), max(q4, q5)) # whiskers cannot be inside the box
-        end        
-        # Box
-        if notch
-            push!(xsegs, m, lw, rw, m, m)       # lower T
-            push!(xsegs, l, l, L, R, r, r, l) # lower box
-            push!(xsegs, l, l, L, R, r, r, l) # upper box
-            push!(xsegs, m, lw, rw, m, m)       # upper T
-
-            push!(ysegs, q1, q1, q1, q1, q2)             # lower T
-            push!(ysegs, q2, q3-n, q3, q3, q3-n, q2, q2) # lower box
-            push!(ysegs, q4, q3+n, q3, q3, q3+n, q4, q4) # upper box
-            push!(ysegs, q5, q5, q5, q5, q4)             # upper T
-        else
-            push!(xsegs, m, lw, rw, m, m)         # lower T
-            push!(xsegs, l, l, r, r, l)         # lower box
-            push!(xsegs, l, l, r, r, l)         # upper box
-            push!(xsegs, m, lw, rw, m, m)         # upper T
-
-            push!(ysegs, q1, q1, q1, q1, q2)    # lower T
-            push!(ysegs, q2, q3, q3, q2, q2)    # lower box
-            push!(ysegs, q4, q3, q3, q4, q4)    # upper box
-            push!(ysegs, q5, q5, q5, q5, q4)    # upper T
         end
+        # Box
+        push!(xsegs, m, lw, rw, m, m)       # lower T
+        push!(ysegs, q1, q1, q1, q1, q2)             # lower T
+        push!(
+            texts,
+            "Lower fence: $q1",
+            "Lower fence: $q1",
+            "Lower fence: $q1",
+            "Lower fence: $q1",
+            "Q1: $q2",
+            "",
+        )
+        if notch
+            push!(xsegs, l, l, L, m, R, r, r, l) # lower box
+            push!(xsegs, l, l, L, R, r, r, l) # upper box
+
+            push!(ysegs, q2, q3 - n, q3, q3, q3, q3 - n, q2, q2) # lower box
+            push!(
+                texts,
+                "Q1: $q2",
+                "Median: $q3 ± $n",
+                "",
+                "Median: $q3 ± $n",
+                "Median: $q3 ± $n",
+                "Median: $q3 ± $n",
+                "Q1: $q2",
+                "Q1: $q2",
+                "",
+            )
+            push!(ysegs, q4, q3 + n, q3, q3, q3 + n, q4, q4) # upper box
+            push!(
+                texts,
+                "Q3: $q4",
+                "Median: $q3 ± $n",
+                "Median: $q3 ± $n",
+                "",
+                "Median: $q3 ± $n",
+                "Q3: $q4",
+                "Q3: $q4",
+                "",
+            )
+        else
+            push!(xsegs, l, l, m, r, r, l)         # lower box
+            push!(xsegs, l, l, r, r, l)         # upper box
+            push!(ysegs, q2, q3, q3, q3, q2, q2)    # lower box
+            push!(
+                texts,
+                "Q1: $q2",
+                "Median: $q3",
+                "Median: $q3",
+                "Median: $q3",
+                "Q1: $q2",
+                "Q1: $q2",
+                "",
+            )
+            push!(ysegs, q4, q3, q3, q4, q4)    # upper box
+            push!(texts, "Q3: $q4", "Median: $q3", "Median: $q3", "Q3: $q4", "Q3: $q4", "")
+        end
+        push!(xsegs, m, lw, rw, m, m)             # upper T
+        push!(ysegs, q5, q5, q5, q5, q4)             # upper T
+        push!(
+            texts,
+            "Upper fence: $q5",
+            "Upper fence: $q5",
+            "Upper fence: $q5",
+            "Upper fence: $q5",
+            "Q3: $q4",
+            "",
+        )
+
     end
 
     # To prevent linecolor equal to fillcolor (It makes the median visible)
@@ -99,24 +157,35 @@ notch_width(q2, q4, N) = 1.58 * (q4-q2)/sqrt(N)
     if outliers
         primary := false
         @series begin
-            seriestype  := :scatter
+            seriestype := :scatter
             if get!(plotattributes, :markershape, :circle) == :none
-                	plotattributes[:markershape] = :circle
+                plotattributes[:markershape] = :circle
             end
 
-            fillrange   := nothing
-            x           := outliers_x
-            y           := outliers_y
-            primary     := true
+            fillrange := nothing
+            x := outliers_x
+            y := outliers_y
+            primary := true
             ()
         end
     end
+
+    # Hover
+    @series begin
+        seriestype := :path
+        x := xsegs.pts
+        y := ysegs.pts
+        hover := texts
+        linewidth := 0
+    end
+
 
     seriestype := :shape
     x := xsegs.pts
     y := ysegs.pts
     ()
 end
+
 Plots.@deps boxplot shape scatter
 
 
