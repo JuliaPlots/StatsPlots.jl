5508790e03ed6b26d00a02e9a6e9de17d34d7c62
Nicholas Bauer <nicholasbauer@outlook.com>
2021-03-18 16:15:10 -0400

    Moved boxplot shorthand from Plots.jl

--- src/boxplot.jl
+++ src/boxplot.jl
@@ -238,4 +238,22 @@ recipetype(::Val{:groupedboxplot}, args...) = GroupedBoxplot(args)
     x, y
 end
 
+"""
+    boxplot(x, y)
+    boxplot!(x, y)
+Make a box and whisker plot.
+# Keyword arguments
+- `notch`: Bool. Notch the box plot? (false)
+- `range`: Real. Values more than range*IQR below the first quartile
+           or above the third quartile are shown as outliers (1.5)
+- `outliers`: Bool. Show outliers? (true)
+- `whisker_width`: Real or Symbol. Length of whiskers (:match)
+# Example
+```julia-repl
+julia> using StatsPlots
+julia> boxplot(repeat([1,2,3],outer=100),randn(300))
+```
+"""
+@shorthands boxplot
+
 Plots.@deps groupedboxplot boxplot
