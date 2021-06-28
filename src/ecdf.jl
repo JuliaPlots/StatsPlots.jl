d0d2ff0a7733f7af5b05f1e5307f5792313fa97f
Seth Axen <seth.axen@gmail.com>
2021-06-28 17:05:52 +0200

    Only compute unique if necessary

--- src/ecdf.jl
+++ src/ecdf.jl
@@ -2,13 +2,21 @@
 # ---------------------------------------------------------------------------
 # empirical CDF
 
+function allsortedunique(x)
+    return all(eachindex(x)[1:end-1]) do i
+        @inbounds x[i] < x[i + 1]
+    end
+end
+
 @recipe function f(ecdf::StatsBase.ECDF; npoints=nothing)
     seriestype --> :steppost
     legend --> :topleft
     if npoints !== nothing
         x = [-Inf; range(extrema(ecdf)...; length=npoints)]
     else
-        x = [-Inf; unique(ecdf.sorted_values)]
+        xnonunique = ecdf.sorted_values
+        xunique = allsortedunique(xnonunique) ? xnonunique : unique(xnonunique)
+        x = [-Inf; xunique]
     end
     y = ecdf(x)
     x, y
