2ad96c95388623a0a4c07924502c9329b90a00ee
Seth Axen <seth.axen@gmail.com>
2021-06-28 17:11:52 +0200

    Add ecdfplot docstring

--- src/ecdf.jl
+++ src/ecdf.jl
@@ -22,7 +22,24 @@ end
     x, y
 end
 
+
+"""
+    ecdfplot(x; npoints = nothing)
+
+Plot the empirical cumulative distribution function (ECDF) of `x`.
+
+By default, the ECDF is evaluated at the unique points in `x`. If `npoints` is provided,
+it is instead evaluated on a uniform grid of length `npoints` between the extrema of `x`.
+This is useful when `x` is large.
+
+```julia-repl
+julia> ecdfplot(randn(100))
+
+julia> ecdfplot(randn(1_000_000); npoints=100, seriestype=:path)
+```
+"""
 @userplot ECDFPlot
+
 recipetype(::Val{:ecdfplot}, args...) = ECDFPlot(args)
 @recipe function f(p::ECDFPlot)
     x = p.args[1]
