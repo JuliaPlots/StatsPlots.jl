2fd7e7ea8947b028cb939bc71fc4a1795b334126
Seth Axen <seth.axen@gmail.com>
2022-02-05 13:39:28 +0100

    Use new predict instead of deprecated transform

--- src/ordinations.jl
+++ src/ordinations.jl
@@ -1,8 +1,8 @@
-@recipe function f(mds::MDS{<:Real}; mds_axes=(1,2))
+@recipe function f(mds::MultivariateStats.MDS{<:Real}; mds_axes=(1,2))
     length(mds_axes) in [2,3] || throw(ArgumentError("Can only accept 2 or 3 mds axes"))
     xax = mds_axes[1]
     yax = mds_axes[2]
-    tfm = collect(transform(mds)')
+    tfm = collect(MultivariateStats.predict(mds)')
 
     xlabel --> "MDS$xax"
     ylabel --> "MDS$yax"
