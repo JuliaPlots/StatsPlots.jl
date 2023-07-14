ce76cf60e81d555e813016546743a1f7c469efc1
Seth Axen <seth@sethaxen.com>
2023-07-14 20:11:23 +0200

    Update src/boxplot.jl

--- src/boxplot.jl
+++ src/boxplot.jl
@@ -32,7 +32,7 @@ notch_width(q2, q4, N) = 1.58 * (q4 - q2) / sqrt(N)
     isnothing(bw) && (bw = 0.8)
     @assert whisker_width === :match || whisker_width == :half || whisker_width >= 0 "whisker_width must be :match, :half, or a positive number"
     ww = whisker_width === :match ? bw : whisker_width == :half ? bw / 2 : whisker_width
-    for (i, glabel) in enumerate(sort(glabels, by = sort_labels_by))
+    for (i, glabel) in enumerate(sort(glabels; by = sort_labels_by))
         # filter y
         values = y[filter(i -> _cycle(x, i) == glabel, 1:length(y))]
 
